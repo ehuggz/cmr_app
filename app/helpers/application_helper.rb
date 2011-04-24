a609eae7c78b2d0d75c0e3dd0e9246747507d5bb
Eric Huggins <eric.huggins1@gmail.com>
2011-04-24 18:26:57 -0400

    C5 Exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
  # Return a title on a per-page basis.
- def title
-   base_title = "Construction Market Report Sample App"
-   if @title.nil?
-     base_title
-   else
-     "#{base_title} | #{@title}"
-   end
- end
+  def logo
+    image_tag("CMR.png", :alt => "Sample App", :class => "round")
+  end
+
+  def title
+    base_title = "Construction Market Report Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
